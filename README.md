07ea4489238f6766dfdf2139d053de8b2556d728
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-06-21 11:48:23 -0700

    almost working

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # meshblu-connector-configurator-windows-registry
-Get connector information from the windows registry of all places
+Configure Meshblu Connectors using PM2 using a directory tree of meshblu.json
+
+
+## Build on ARM
+```
+../dockcross-node/dockcross-linux-armv7 --image octoblu/dockcross-node-linux-armv7 bash -c 'export HOME=/tmp/cache && yarn install && yarn package -- --target node8-linux-armv7'; and cp deploy/command deploy/meshblu-connector-configurator-windows-registry_1.0-1/usr/share/meshblu-connector-pm2/configurators/meshblu-connector-configurator-windows-registry
+rm -rf meshblu-connector-configurator-windows-registry_1.0-1/DEBIAN meshblu-connector-configurator-windows-registry_1.0-1/etc; cp -rfp ../.installer/debian/* meshblu-connector-configurator-windows-registry_1.0-1 ; and dpkg --build meshblu-connector-configurator-windows-registry_1.0-1; and scp meshblu-connector-configurator-windows-registry_1.0-1.deb pi@192.168.100.141:; ssh pi@192.168.100.141 "sudo dpkg -i meshblu-connector-configurator-windows-registry_1.0-1.deb"
+cd deploy/
+```
---
+++ command.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+const dashdash = require("dashdash")
+const path = require("path")
+const util = require("util")
+const fs = require("fs")
+const chalk = require("chalk")
+const { MeshbluConnectorConfigurator } = require("./lib/configurator")
+
+const CLI_OPTIONS = [
+  {
+    name: "version",
+    type: "bool",
+    help: "Print connector version and exit.",
+  },
+  {
+    names: ["help", "h"],
+    type: "bool",
+    help: "Print this help and exit.",
+  },
+  {
+    names: ["connector-home"],
+    type: "string",
+    env: "MESHBLU_CONNECTOR_HOME",
+    help: "Base location of meshblu connectors",
+    helpArg: "PATH",
+  },
+  {
+    names: ["pm2-home"],
+    type: "string",
+    env: "PM2_HOME",
+    help: "Base location of meshblu-connector-pm2",
+    helpArg: "PATH",
+  },
+]
+
+class MeshbluConnectorConfiguratorCommand {
+  constructor(options) {
+    if (!options) options = {}
+    var { argv, cliOptions } = options
+    if (!cliOptions) cliOptions = CLI_OPTIONS
+    if (!argv) return this.die(new Error("MeshbluConnectorConfiguratorCommand requires options.argv"))
+    this.argv = argv
+    this.cliOptions = cliOptions
+    this.parser = dashdash.createParser({ options: this.cliOptions })
+  }
+
+  parseArgv({ argv }) {
+    try {
+      var opts = this.parser.parse(argv)
+    } catch (e) {
+      return {}
+    }
+
+    if (opts.help) {
+      console.log(`usage: meshblu-connector-configurator-windows-registry [OPTIONS]\noptions:\n${this.parser.help({ includeEnv: true })}`)
+      process.exit(0)
+    }
+
+    if (opts.version) {
+      console.log(this.packageJSON.version)
+      process.exit(0)
+    }
+
+    return opts
+  }
+
+  async run() {
+    const options = this.parseArgv({ argv: this.argv })
+    const { connector_home, pm2_home } = options
+    var errors = []
+    if (!connector_home) errors.push(new Error("MeshbluConnectorCommand requires --connector-home or MESHBLU_CONNECTOR_HOME"))
+    if (!pm2_home) errors.push(new Error("MeshbluConnectorCommand requires --pm2-home or PM2_HOME"))
+
+    if (errors.length) {
+      console.log(`usage: meshblu-connector-configurator-windows-registry [OPTIONS]\noptions:\n${this.parser.help({ includeEnv: true })}`)
+      errors.forEach(error => {
+        console.error(chalk.red(error.message))
+      })
+      process.exit(1)
+    }
+
+    const configurator = new MeshbluConnectorConfigurator({ connectorHome: connector_home, pm2Home: pm2_home })
+    try {
+      await configurator.configurate()
+    } catch (error) {
+      this.die(error)
+    }
+    // process.exit(0)
+  }
+
+  die(error) {
+    console.error(chalk.red("Meshblu Connector Installer Command: error: %s", error.message))
+    process.exit(1)
+  }
+}
+
+const command = new MeshbluConnectorConfiguratorCommand({ argv: process.argv })
+command.run().catch(error => {
+  console.error(error)
+})
